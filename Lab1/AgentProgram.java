2e54a464f39cba579ccf0c9e864b4e086f80bb73
Ngan22130178 <146084366+Ngan22130178@users.noreply.github.com>
2023-10-03 00:12:49 +0700

    Add files via upload

---
+++ Lab1/AgentProgram.java
@@ -0,0 +1,13 @@
+package chapter2.agent_AB;
+
+public class AgentProgram {
+
+	public Action execute(Percept p) {// location, status
+		if (p.getLocationState() == Environment.LocationState.DIRTY)
+			return Environment.SUCK_DIRT;
+		else if (p.getAgentLocation() == Environment.LOCATION_A)
+			return Environment.MOVE_RIGHT;
+
+        return Environment.MOVE_LEFT;
+    }
+}
